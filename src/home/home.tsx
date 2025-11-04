8449fe9dd1492b88b8c0cf5ac9aaa4dec7bf983f
Lorcarlos <carperez45@uan.edu.co>
2025-11-03 21:13:32 -0500

    feat(front): crear estructura base del frontend
    Se agregan carpetas src, components, assets y pages.

---
+++ src/home/home.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React + TypeScript 👋</h1>
+      <p>Esta es la estructura base de un componente .tsx</p>
+    </div>
+  );
+}
+
+export default App;
