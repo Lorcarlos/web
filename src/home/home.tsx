6c3b6b9cc285e359ab90d4801939e50c3502264f
karnos58 <omaralejo1999ail.com@outlook.com>
2025-11-03 22:11:54 -0500

    Mi primer push

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,10 +1,12 @@
 // src/App.tsx
 import React from "react";
+import "./home.css";
 
 function App() {
   return (
-    <div>
-      <h1>Hello React + TypeScript 👋</h1>
+    <div className="conter">
+   
+      <h1 className="text">Hello React + TypeScript </h1>
       <p>Esta es la estructura base de un componente .tsx</p>
     </div>
   );
